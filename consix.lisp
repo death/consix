697e7ed259858d342eb4b4bedd562533dd69cf7d
death <death@nessers.org>
2010-03-21 17:23:17 +0200

    we start with a grid and a player

--- consix.lisp
+++ consix.lisp
@@ -5,6 +5,131 @@
 (in-package #:consix)
 
 
+;;;; Grid
+
+(defconstant cell-unclaimed #x00)
+(defconstant cell-claimed   #x01)
+(defconstant cell-claiming  #x02)
+(defconstant cell-edge      #x03)
+
+(defconstant grid-rows 90)
+(defconstant grid-cols 90)
+
+(defconstant cell-width 2)
+(defconstant cell-height 2)
+
+(defconstant grid-x-offset (floor (* grid-cols cell-width) -2))
+(defconstant grid-y-offset (floor (* grid-rows cell-height) -2))
+
+(deftype cell () '(unsigned-byte 8))
+
+(defclass grid ()
+  ((total-unclaimed :accessor total-unclaimed)
+   (current-unclaimed :accessor current-unclaimed)
+   (cells :accessor cells)))
+
+(defmethod initialize-instance :after ((grid grid) &rest initargs)
+  (declare (ignore initargs))
+  (setf (cells grid) (make-cells))
+  (setf (total-unclaimed grid) (count cell-unclaimed (cells grid)))
+  (setf (current-unclaimed grid) (total-unclaimed grid)))
+
+(defun make-cells ()
+  (let ((cells (make-array (* grid-rows grid-cols)
+                           :element-type 'cell
+                           :initial-element cell-unclaimed)))
+    (macrolet ((ref (row col) `(aref cells (+ ,col (* ,row grid-cols)))))
+      (dotimes (row grid-rows)
+        (setf (ref row 0) cell-edge)
+        (setf (ref row (1- grid-cols)) cell-edge))
+      (dotimes (col grid-cols)
+        (setf (ref 0 col) cell-edge)
+        (setf (ref (1- grid-rows) col) cell-edge)))
+    cells))
+
+(defun cell-ref (row col grid)
+  (aref (cells grid) (+ (* row grid-cols) col)))
+
+(defun (setf cell-ref) (new-value row col grid)
+  (setf (aref (cells grid) (+ (* row grid-cols) col)) new-value))
+
+(defun cell-center-position (row col)
+  (vec (+ grid-x-offset (* col cell-width) (floor cell-width 2))
+       (+ grid-y-offset (* row cell-height) (floor cell-height 2))))
+
+(defun cell-row/col (pos)
+  (values (floor (- (y pos) grid-y-offset) cell-height)
+          (floor (- (x pos) grid-x-offset) cell-width)))
+
+(defun valid-cell-p (row col)
+  (and (>= row 0) (< row grid-rows)
+       (>= col 0) (< col grid-cols)))
+
+(defmethod render ((grid grid))
+  (loop for i from 0
+        for cell across (cells grid)
+        do (multiple-value-bind (row col)
+               (floor i grid-rows)
+             (multiple-value-bind (r g b a)
+                 (case cell
+                   (#.cell-unclaimed (values 0.0 0.0 0.0 0.0))
+                   (#.cell-claimed   (values 0.0 1.0 0.0 0.5))
+                   (#.cell-claiming  (values 1.0 0.0 0.0 0.5))
+                   (#.cell-edge      (values 0.0 1.0 0.0 1.0)))
+               (gl:color r g b a)
+               (gl:with-primitive :quads
+                 (let ((sx (+ grid-x-offset (* col cell-width) 0.1))
+                       (sy (+ grid-y-offset (* row cell-height) 0.1))
+                       (ex (+ grid-x-offset (* (1+ col) cell-width) -0.1))
+                       (ey (+ grid-y-offset (* (1+ row) cell-height) -0.1)))
+                   (gl:vertex sx sy)
+                   (gl:vertex ex sy)
+                   (gl:vertex ex ey)
+                   (gl:vertex sx ey)))))))
+
+
+;;;; Halo
+
+(defclass halo ()
+  ((value :initform 0.0 :accessor halo-value)
+   (step :initform 0.05 :accessor halo-step)))
+
+(defun halo-update (halo)
+  (let ((new-value (+ (halo-value halo) (halo-step halo))))
+    (cond ((or (< new-value 0.0) (> new-value 1.0))
+           (setf (halo-step halo) (- (halo-step halo))))
+          (t (setf (halo-value halo) new-value)))))
+
+
+;;;; Player
+
+(defclass player ()
+  ((pos :accessor pos)
+   (row :initarg :row :accessor row)
+   (col :initarg :col :accessor col)
+   (grid :initarg :grid :accessor grid)
+   (halo :initform (make-instance 'halo) :accessor halo)))
+
+(defmethod initialize-instance :after ((player player) &rest initargs)
+  (declare (ignore initargs))
+  (let ((row (row player))
+        (col (col player)))
+    (setf (pos player) (cell-center-position row col))))
+
+(defmethod update ((player player))
+  (halo-update (halo player)))
+
+(defmethod render ((player player))
+  (gl:with-pushed-matrix
+    (with-vec (x y (pos player))
+      (gl:translate x y 0.0)
+      (dotimes (i 5)
+        (gl:color 0.0 0.0 1.0 (* i 0.2))
+        (draw-circle (- 5 i) 30 t))
+      (gl:color 0.0 0.0 1.0 (halo-value (halo player)))
+      (draw-circle 5))))
+
+
 ;;;; Game
 
 (defclass consix-window (game-window)
@@ -12,9 +137,10 @@
   (:default-initargs
    :title "Consix"))
 
-(define-level consix)
+(define-level (consix :test-order '(player t))
+  (grid :named grid)
+  (player :row (1- grid-rows) :col (floor grid-cols 2) :grid grid))
 
 (defun game ()
   (glut:display-window
-   (make-instance 'consix-window
-                  :world (make-instance 'consix))))
+   (make-instance 'consix-window :world 'consix)))
